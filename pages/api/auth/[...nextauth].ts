74f73a2d4455a71db9ec3fcfc4cdcd9582fc15d5
SanoffDEV <picotmathis42@gmail.com>
2025-03-21 18:34:34 +0100

    o

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,33 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/src/lib/prisma";
-
 import GoogleProvider from "next-auth/providers/google";
+
+// Vérifie les variables d’environnement
+if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
+  throw new Error(
+    "Les variables GOOGLE_CLIENT_ID ou GOOGLE_CLIENT_SECRET ne sont pas définies."
+  );
+}
+
+if (!process.env.NEXTAUTH_URL) {
+  throw new Error("NEXTAUTH_URL n’est pas défini.");
+}
+
+if (!process.env.NEXTAUTH_SECRET) {
+  throw new Error("NEXTAUTH_SECRET n’est pas défini.");
+}
 
 export const authConfig = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: process.env.GOOGLE_SCOPE || "openid email profile",
+        },
+      },
     }),
   ],
   adapter: PrismaAdapter(prisma),
@@ -17,7 +36,13 @@ export const authConfig = {
       session.user.id = user.id;
       return session;
     },
+    redirect: async ({ url, baseUrl }) => {
+      console.log("Redirect URL:", url);
+      console.log("Base URL:", baseUrl);
+      return url;
+    },
   },
+  secret: process.env.NEXTAUTH_SECRET,
 } satisfies NextAuthOptions;
 
 export default NextAuth(authConfig);
