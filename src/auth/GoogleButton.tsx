854531b34ec29490699cecc0ca250bcf8523cc36
SanoffDEV <picotmathis42@gmail.com>
2025-03-21 20:45:03 +0100

    o

--- src/auth/GoogleButton.tsx
+++ src/auth/GoogleButton.tsx
@@ -14,7 +14,13 @@ export function GoogleButton() {
       onClick={handleLogin}
       className="mb-3 flex items-center justify-center w-full gap-2 bg-gray-900 hover:bg-gray-800 text-white rounded-md py-2"
     >
-      <Image src="/google.svg" alt="google" className="w-6 h-6" />
+      <Image
+        src="/google.svg"
+        alt="google"
+        width={48}
+        height={48}
+        className="w-6 h-6"
+      />
       Login with Google
     </Button>
   );
