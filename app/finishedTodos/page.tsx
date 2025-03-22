7e51d0ee2180fc11a065a8b21fb4d194ab976287
SanoffDEV <picotmathis42@gmail.com>
2025-03-22 11:35:50 +0100

    o

--- app/finishedTodos/page.tsx
+++ app/finishedTodos/page.tsx
@@ -17,7 +17,7 @@ export default async function FinishedTodos() {
         <div>{session ? <DropDownMenu /> : <LoginButton />}</div>
       </header>
       <header className="absolute top-5 right-5"></header>
-      <div className="fixed xl:left-4  left-2 top-8">
+      <div className="fixed xl:left-4  left-2 top-2">
         <Sidebar />
       </div>
 
