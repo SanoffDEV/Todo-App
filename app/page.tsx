dfbd9232ea6ad85869c7b6456ce2f5c1a80be1f3
SanoffDEV <picotmathis42@gmail.com>
2025-03-22 11:34:22 +0100

    o

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default async function Home() {
       <header className="fixed top-5 right-5">
         <div>{session ? <DropDownMenu /> : <LoginButton />}</div>
       </header>
-      <div className="fixed xl:left-4  left-2 ">
+      <div className="fixed xl:left-4  left-2 top-8 ">
         <Sidebar />
       </div>
 
